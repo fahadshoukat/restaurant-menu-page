58fb301c6f7cd8af708d502ef6096f272caa1980
fahadshoukat <fraza6970@gmail.com>
2021-11-29 22:46:48 +0500

    createMenuItemsArray

---
+++ menu.js
@@ -0,0 +1,75 @@
+const menu = [
+    {
+        id: 1,
+        title: "Toast With Sunny Side Egg",
+        category: "breakfast",
+        price: 10,
+        img: "images/breakfast/bread-with-sunny-side-up-egg.jpg",
+        desc: "Made on whole grain toast with mashed avocado, a runny egg and a few dashes of hot sauce – 5 ingredients, 5 minutes to make, it doesn’t get better than that!"
+    },
+    {
+        id: 2,
+        title: "Blueberry Toast",
+        category: "breakfast",
+        price: 15,
+        img: "images/breakfast/toast-bread-with-blueberry.jpg",
+        desc: "Great cereal with a great taste. Being able to purchase in multiples and send to a Grandson in the Air Force is the most important thing as this is his favorite cereal."
+    },
+    {
+        id: 3,
+        title: "Toast With Fried Egg",
+        category: "breakfast",
+        price: 12,
+        img: "images/breakfast/toasted-bread-with-fried-egg.jpg",
+        desc: "Yum, definitely. I love wilted spinach with eggs. My fave too, Emily! Try it with a layer of freshly wilted spinach between the mayo and the egg -- yum!"
+    },
+    {
+        id: 4,
+        title: "Salad With Lemon",
+        category: "lunch",
+        price: 19,
+        img: "images/lunch/bowls-of-salad-with-lemon.jpg",
+        desc: "I made this for a quick appetizer! My guests loved it! I will definitely make this again! Give this recipe a try! It will not disappoint!!"
+    },
+    {
+        id: 5,
+        title: "Fruits Salad",
+        category: "lunch",
+        price: 20,
+        img: "images/lunch/fruits-salads.jpg",
+        desc: "Great fruit salad! The sauce was enough to dress up the salad without adding a ton of extra calories. I took this to a party and everyone thought it was very good."
+    },
+    {
+        id: 6,
+        title: "Vegetables Burger",
+        category: "lunch",
+        price: 11,
+        img: "images/lunch/burger-bun-with-vegetables.jpg",
+        desc: "Our Gourmet Vegan burgers are certified gluten free and 100% organic. They are based on Quinoa and vegetables and were nominated in May by the Vegetarian Times for Best Veggie Burger."
+    },
+    {
+        id: 7,
+        title: "Chocolate Cookie Frappe",
+        category: "shakes",
+        price: 7,
+        img: "images/shakes/Chocolate-Cookies-Frappe.jpg",
+        desc: "I wanted to love it, but it was like the cookie monster had been constipated and found some exlax and used my cup as a toilet because the bathroom in my Starbucks was so filthy."
+    },
+    {
+        id: 8,
+        title: "Double Dutch Ice-cream",
+        category: "shakes",
+        price: 9,
+        img: "images/shakes/Double-Dutch-Ice-Cream.jpg",
+        desc: "A creamy and mouth-watering homemade ice cream idea from Alaska Crema-Asada, this is a perfect ice cream base to use. With this 2-in-1 Alaska Crema-Asada you can make different ice cream flavors of your choice."
+    },
+    {
+        id: 9,
+        title: "Red Respberries",
+        category: "shakes",
+        price: 13,
+        img: "images/shakes/Red-Respberries.jpg",
+        desc: "Red raspberries are a common and important fruit in the Western diet due to their content of essential nutrients and beneficial phytochemicals. Anthocyanins and ellagitannins are polyphenolic compounds and the major antioxidant phytochemicals present in raspberries."
+    },
+]
+
